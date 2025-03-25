b785060e1d2bdb77ae99700925352045048cef5a
Kevin Nielsen <kevinanielsen@outlook.com>
2025-03-25 10:31:23 +0100

    initialize frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import './App.css'
+import { Test } from './components/test'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>P2</h1>
+      <Test></Test>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/components/test.jsx
@@ -0,0 +1,10 @@
+export const Test = () => {
+  return (
+    <>
+      <h2>Det her er en test</h2>
+      <h2>Det her er en test</h2>
+      <h2>Det her er en test</h2>
+      <h2>Det her er en test</h2>
+    </>
+  );
+};
