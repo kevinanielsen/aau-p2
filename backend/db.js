2e56c0dbd043772a6dcd33de8d6633c901b1a82a
Kevin Nielsen <kevinanielsen@outlook.com>
2025-04-05 16:39:52 +0200

    add log statement when db connection is established

--- backend/db.js
+++ backend/db.js
@@ -8,8 +8,7 @@ const mongoDB = process.env.MONGO_URI;
 
 async function startDB(){
   await mongoose.connect(mongoDB);
+  console.log("Database connection established!");
 }
 
 export {startDB};
-
-
