1250f620c49edb1db5a7285f384ab4545f378537
Kevin Nielsen <kevinanielsen@outlook.com>
2025-04-05 16:41:02 +0200

    implement signup controller

---
+++ backend/controllers/signUpController.js
@@ -0,0 +1,24 @@
+import { User } from "../models/users";
+import { createSecretToken } from "../util/secretToken";
+
+export const signUp = async (req, res, next) => {
+  try {
+    const { email, password, username, createdAt } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      res.status(400).json({ message: "User already exists" });
+    }
+    const user = await User.create({ email, password, username, createdAt });
+    const token = createSecretToken(user._id);
+    res.cookie("token", token, {
+      withCredentials: true,
+      httpOnly: false,
+    });
+    res
+      .status(201)
+      .json({ message: "User signed up successfully", success: true, user });
+    next();
+  } catch (error) {
+    console.error(error);
+  }
+};
--- backend/controllers/signinController.js
+++
@@ -1,5 +0,0 @@
-const signup_post = (req, res) => {
-    res.send("Sign up page");
-}
-
-export { signup_post }
